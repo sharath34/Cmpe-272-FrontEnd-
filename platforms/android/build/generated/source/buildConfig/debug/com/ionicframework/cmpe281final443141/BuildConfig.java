92fe1e43535db440a7d9b75821468304ce3923c3
sharath34 <sharathchandra.muddana@sjsu.edu>
2015-04-29 20:57:31 -0700

    first commit

---
+++ platforms/android/build/generated/source/buildConfig/debug/com/ionicframework/cmpe281final443141/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.ionicframework.cmpe281final443141;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.ionicframework.cmpe281final443141";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 10;
+  public static final String VERSION_NAME = "";
+}
